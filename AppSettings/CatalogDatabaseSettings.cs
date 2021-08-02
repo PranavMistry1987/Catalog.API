ee48e7b98dee287b00d4d98e255e08c719347c78
inmisp08 <pranav.mistry@ingrammicro.com>
2021-08-02 23:18:56 +0530

    Add project files.

---
+++ AppSettings/CatalogDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.AppSettings.Interfaces;
+using System;
+
+namespace Catalog.API.AppSettings
+{
+    public class CatalogDatabaseSettings : ICatalogDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
